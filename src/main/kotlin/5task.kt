7f246f28abf76ef81a8a8be16bffd212c339ba76
mementosss <144599371+mementosss@users.noreply.github.com>
2023-10-31 20:55:28 +0300

    commit

--- src/main/kotlin/5task.kt
+++ src/main/kotlin/5task.kt
@@ -1,14 +1,14 @@
 interface Movable {
     fun move( dx: Int, dy: Int)
 }
-internal class Point(private var x: Int, private var y: Int) : Movable {
+internal class Point(var x: Int,var y: Int) : Movable {
     override fun move(dx: Int, dy: Int) {
         x += dx
         y += dy
     }
 }
 
-internal class CloudOfPoints(private val points: ArrayList<Point>) : Movable {
+internal class CloudOfPoints(val points: ArrayList<Point>) : Movable {
     override fun move(dx: Int, dy: Int) {
         for (point in points) {
             point.move(dx, dy)
